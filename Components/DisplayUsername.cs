8c8c8da7d1d0c77f3b91ebb7a9eb497d79decd72
Rylie DuVal <rylieduval10@gmail.com>
2026-09-04 10:04:30 -0500

    DisplayUsername links to the profile

--- Components/DisplayUsername.cs
+++ Components/DisplayUsername.cs
@@ -11,11 +11,31 @@ namespace RotoMonsterUI
             _input = input;
         }
 
-        public string RenderAvatar()
+        private string DisplayText()
         {
-            var displayText = !string.IsNullOrEmpty(_input.Username)
+            return !string.IsNullOrEmpty(_input.Username)
                 ? _input.Username
                 : _input.UserId.HasValue ? $"#{_input.UserId}" : "";
+        }
+
+        private string ProfileHref()
+        {
+            if (!_input.LinkToProfile) return null;
+            if (string.IsNullOrEmpty(_input.ProfileUrl)) return null;
+
+            if (_input.UserId.HasValue)
+                return _input.ProfileUrl + "?userid=" + _input.UserId.Value;
+
+            if (!string.IsNullOrEmpty(_input.Username))
+                return _input.ProfileUrl + "?username="
+                    + System.Uri.EscapeDataString(_input.Username);
+
+            return null;
+        }
+
+        public string RenderAvatar()
+        {
+            var displayText = DisplayText();
 
             if (!string.IsNullOrEmpty(_input.AvatarUrl))
             {
@@ -35,17 +55,20 @@ namespace RotoMonsterUI
 
         public string Render()
         {
-            var displayText = !string.IsNullOrEmpty(_input.Username)
-                ? _input.Username
-                : _input.UserId.HasValue ? $"#{_input.UserId}" : "";
+            var displayText = DisplayText();
+            var href = ProfileHref();
 
             var wrapper = new HtmlTag("span").AddClass("display-username-wrap");
 
             if (_input.ShowAvatar)
                 wrapper.AppendHtml(RenderAvatar());
 
-            var tag = new HtmlTag("span")
-                .AddClass("display-username");
+            var tag = href == null
+                ? new HtmlTag("span").AddClass("display-username")
+                : new HtmlTag("a").AddClass("display-username").Attr("href", href);
+
+            if (href != null && !string.IsNullOrEmpty(_input.ProfileTarget))
+                tag.Attr("target", _input.ProfileTarget).Attr("rel", "noopener");
 
             if (!string.IsNullOrEmpty(_input.CssClass))
                 tag.AddClass(_input.CssClass);
--- Models/DisplayUsernameInput.cs
+++ Models/DisplayUsernameInput.cs
@@ -8,5 +8,9 @@ namespace RotoMonsterUI
         public string AvatarUrl { get; set; }
         public bool ShowAvatar { get; set; } = false;
         public int? TotalPostCount { get; set; }
+
+        public bool LinkToProfile { get; set; } = true;
+        public string ProfileUrl { get; set; } = "MessageUserProfile.aspx";
+        public string ProfileTarget { get; set; }
     }
 }
